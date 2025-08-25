19aa8fb3b19fbc806a4fb88f83d5525d2492b1a0
Siddhartha Pathak <sidx404@gmail.com>
2025-08-25 15:13:43 +0530

    flask

--- 2flask/main.py
+++ 2flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ def index():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+@app.route('/form', methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
